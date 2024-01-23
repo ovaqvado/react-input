1007be53837d2261a87ebe8330cd1e83da4fa2dc
ovaqvado <likeriklohhg@gmail.com>
2024-01-23 19:23:50 +0300

    import input my-project react

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+class InputComponent extends React.Component {
+  render() {
+    return (
+      <input type="text" placeholder="Enter text" />
+    );
+  }
+}
+ 
+export default InputComponent;
